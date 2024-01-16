2553fcc98be4f1733bdce36fab4023b00079932b
Md Najrul Islam <mdnajrulislam.4738@gmail.com>
2024-01-16 17:53:58 +0600

    I have install firebase and created  AuthProvider. I have also added signIn and signUp page.

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -2,27 +2,29 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayOut from "../LayOut/MainLayOut";
 import Home from "../Home/Home";
 import Error from "../Error/Error";
-
+import SignIn from "../Components/Login/SignIn";
+import Register from "../Components/Login/Register";
 
 const Router = createBrowserRouter([
-    
-    {
-        path :"/",
-       element :<MainLayOut></MainLayOut> ,
-       errorElement :<Error></Error>,
-       children :[
-        {
-            path : "/",
-            element : <Home></Home>,
-           
-        },
-        
-       
-    
-
-        
-       ]}
-    
-])
+  {
+    path: "/",
+    element: <MainLayOut></MainLayOut>,
+    errorElement: <Error></Error>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/signIn",
+        element: <SignIn></SignIn>,
+      },
+      {
+        path: "/signUp",
+        element: <Register></Register>,
+      },
+    ],
+  },
+]);
 
 export default Router;
