3a052880522444982c21273983636f4d40475abb
Md Najrul Islam <mdnajrulislam.4738@gmail.com>
2024-02-22 00:05:17 +0600

    I have Updated font-family and fixed warning from index.css file and updated footer ssection

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,12 +3,14 @@ import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="text-violet-900 font-Ubuntu font-semibold ">
+    <footer className="bg-[#1E1E1E] text-white  ">
       <div className="p-10 max-w-[1200px] mx-auto ">
         <div className=" footer py-10 ">
           <aside>
             <div>
-              <h3 className="text-2xl md:text-3xl my-3">E - Cash </h3>
+              <h3 className="text-2xl md:text-3xl my-3 font-semibold">
+                E - Cash{" "}
+              </h3>
             </div>
             <p>Online F - Commerce Platform.</p>
           </aside>
