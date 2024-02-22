090a228c13d7264d52c58a3101108cd43a5712b7
emurmudu <eliasmurmudu@gmail.com>
2024-02-22 17:28:03 +0600

    user's transaction details added on dashboard home

--- src/DashBoard/Admin/AdminHome.jsx
+++ src/DashBoard/Admin/AdminHome.jsx
@@ -1,16 +1,18 @@
 import AdminDailyTotalAmountChart from "./AdminDailyTotalAmountChart";
 import AdminMonthChart from "./AdminMonthChart";
 import Transaction from "./Transaction";
+import TransactionHeader from "./TransactionHeader";
 
 const AdminHome = () => {
   return (
     <div>
       <div className="lg:flex items-center mx-8 justify-between">
-      <AdminMonthChart></AdminMonthChart>
-      <AdminDailyTotalAmountChart></AdminDailyTotalAmountChart>
-    </div>
-    
-    <Transaction></Transaction>
+        <AdminMonthChart></AdminMonthChart>
+        <AdminDailyTotalAmountChart></AdminDailyTotalAmountChart>
+      </div>
+
+      <Transaction></Transaction>
+      <TransactionHeader></TransactionHeader>
     </div>
   );
 };
